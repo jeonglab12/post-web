8f23b5e66b833fae24a6645b5f4847a2db1fac42
Jintaek <weezler0@gmail.com>
2023-10-05 14:31:53 +0900

    Init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: "../post/src/main/resources/static",
+  devServer: {
+    proxy: {
+      '/': {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      }
+    }
+  }
 })
